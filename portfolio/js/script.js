edee49d8670c8af331c274fcb262d354c422d0f9
eun4018 <tjdms3273@gmail.com>
2022-03-15 14:13:01 +0900

    포트폴리오 업뎃

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,6 +1,8 @@
-/*헤더 스크롤 색변함*/
+jQuery(function($){
+	var topMenuHeight = $(".nav").outerHeight();
+	$(".nav").menuScroll(topMenuHeight);
+});
 $(function(){
-  var Header = $(".header");
   var change = $("#about");
   var OffsetTop = change.offset().top;
   $(window).resize(function(){
@@ -14,60 +16,75 @@ $(function(){
       }
   })
 })
-
+jQuery.fn.extend({
+    menuScroll: function(offset) {
+        var topMenu = this;
+        var menuItems = $(topMenu).find("a");
+        var lastId;
+        var scrollItems = $(menuItems).map(function() {
+            var item = $($(this).attr("href"));
+            if (item.length) {
+                return item;
+            }
+        });
+	        $(topMenu).on("click", "a", function(e){
+            var href = $(this).attr("href");
+            var offsetTop = $(href).offset().top;
+            $('html, body').stop().animate({ 
+                scrollTop: offsetTop
+            }, 300);
+            e.preventDefault();
+        });
+        $(window).scroll(function(){
+            var fromTop = $(this).scrollTop();
+            var current = $(scrollItems).map(function(){
+                if ($(this).offset().top <= fromTop)
+                return this;
+            });
+            current = current[current.length-1];
+            var idn = current && current.attr('id');
+            if (lastId !== idn) {
+                lastId = idn;
+                $(menuItems)
+                .parent().removeClass("active")
+                .end().filter("[href='#"+idn+"']").parent().addClass("active");
+            }      
+        });
+      }
+});
 $(function(){
-	$('a[href*=#]:not([href=#])').click(function() {
-	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
-    }
-
-	
-  });
-
-
-/* 스크롤 체크 효과 */
-var scroll = window.requestAnimationFrame || function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
-
-function loop() {
-    Array.prototype.forEach.call(elementsToShow, function(element){
-      if (isElementInViewport(element)) {
-        element.classList.add('is-visible');
-      } else {
-        element.classList.remove('is-visible');
-      }
-    });
-
-    scroll(loop);
-}
-loop();
-
-function isElementInViewport(el) {
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
+  var scroll = window.requestAnimationFrame || function(callback){ window.setTimeout(callback, 1000/60)};
+  var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+  
+  function loop() {
+      Array.prototype.forEach.call(elementsToShow, function(element){
+        if (isElementInViewport(element)) {
+          element.classList.add('is-visible');
+        } else {
+          element.classList.remove('is-visible');
+        }
+      });
+  
+      scroll(loop);
   }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
-}
-})
-
-/*modal*/
+  loop();
+  function isElementInViewport(el) {
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+      el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+      (rect.top <= 0
+        && rect.bottom >= 0)
+      ||
+      (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+      ||
+      (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+  }
+});
 function openModal() {
   document.getElementById("myModal").style.display = "block";
   document.getElementById("header").style.display = "none";
